297bf7abce4e66795bca3cb0fdba8d469f3563be
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-16 22:48:13 -0700

    🔧 Add configuration file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="common_task_framework",
+    version="0.1.0",
+    packages=["common_task_framework"],
+    author="Ciencia de Datos • GECI",
+    python_requires='>=3.9'
+)
