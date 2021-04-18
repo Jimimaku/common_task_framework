2a71fd0e4b8f5c7c8c5c26c296b5d127a96b2b0e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-18 16:56:23 -0700

    ♻️ Use class Initializator and Evaluator

--- common_task_framework/main.py
+++ common_task_framework/main.py
@@ -1,17 +1,19 @@
 import typer
-import common_task_framework as ctf
+import common_task_framework
 
 app = typer.Typer()
 
 
 @app.command()
 def init(path_to_complete_dataset: str):
-    ctf.init(path_to_complete_dataset)
+    ctf = common_task_framework.Initializator(path_to_complete_dataset)
+    ctf.init()
 
 
 @app.command()
 def evaluate(path_to_complete_dataset: str, path_to_submission: str):
-    ctf.evaluate(path_to_complete_dataset, path_to_submission)
+    ctf = common_task_framework.Evaluator(path_to_complete_dataset, path_to_submission)
+    ctf.evaluate()
 
 
 if __name__ == "__main__":
