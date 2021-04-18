5c5092224438d91d9d487c742b55b196a1d3de37
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-17 23:35:28 -0700

    🛑 Update failing test for function get_training_dataset

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,7 @@
 import common_task_framework as ctf
 
 path_to_complete_dataset = "tests/test_data/complete_dataset.csv"
+data = ctf.load_complete_dataset(path_to_complete_dataset)
 
 
 def test_load_complete_dataset():
@@ -11,14 +12,19 @@ def test_load_complete_dataset():
 
 
 def test_get_trainig_length():
-    data = ctf.load_complete_dataset(path_to_complete_dataset)
     obtained_length = ctf.get_trainig_length(data)
     expected_length = round(27 * 0.8)
     assert expected_length == obtained_length
 
 
 def test_get_testing_length():
-    data = ctf.load_complete_dataset(path_to_complete_dataset)
     obtained_length = ctf.get_testing_length(data)
     expected_length = round(27 * 0.2)
+    assert expected_length == obtained_length
+
+
+def test_get_trainig_dataset():
+    train = ctf.get_trainig_dataset(data)
+    obtained_length = len(train)
+    expected_length = expected_length = round(27 * 0.8)
     assert expected_length == obtained_length
