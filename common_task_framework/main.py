68bfc2d3099aee46b27233e88b549c5f0cfbfff1
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-18 15:53:36 -0700

    ✨ Add command evaluate

--- common_task_framework/main.py
+++ common_task_framework/main.py
@@ -9,5 +9,10 @@ def init(path_to_complete_dataset: str):
     ctf.init(path_to_complete_dataset)
 
 
+@app.command()
+def evaluate(path_to_complete_dataset: str, path_to_submission: str):
+    ctf.evaluate(path_to_complete_dataset, path_to_submission)
+
+
 if __name__ == "__main__":
     app()
--- spec/cli_spec.sh
+++ spec/cli_spec.sh
@@ -2,5 +2,5 @@
 
 It 'exists'
   When call geci-ctf --help
-  The first line of output should eq "Usage: main.py [OPTIONS] PATH_TO_COMPLETE_DATASET"
+  The first line of output should eq "Usage: main.py [OPTIONS] COMMAND [ARGS]..."
 End
