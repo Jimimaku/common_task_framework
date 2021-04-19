be0f790999ddfa16d8ef2df913bf7082a10dcc15
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-18 19:19:13 -0700

    Update test to verify funtionality

--- spec/cli_spec.sh
+++ spec/cli_spec.sh
@@ -1,6 +1,17 @@
 #shellcheck shell=sh
+Describe "geci-ctf"
+  clean() { \
+            rm --force tests/test_data/example_submission.csv; \
+            rm --force tests/test_data/test.csv; \
+            rm --force tests/test_data/train.csv; \
+            }
+  Before "clean"
+  After "clean"
 
-It 'exists'
-  When call geci-ctf --help
-  The first line of output should eq "Usage: main.py [OPTIONS] COMMAND [ARGS]..."
+  It "partitions the complete dataset"
+    When call geci-ctf init tests/test_data/complete_dataset.csv
+    The file "tests/test_data/example_submission.csv" should be exist
+    The file "tests/test_data/test.csv" should be exist
+    The file "tests/test_data/train.csv" should be exist
+  End
 End
