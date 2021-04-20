480a340f4bc5833e82bf1f47fb62ed27b6bf0295
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-04-19 21:27:01 -0700

    👾 Hunt last mutant

--- spec/cli_spec.sh
+++ spec/cli_spec.sh
@@ -14,4 +14,10 @@ Describe "geci-ctf"
     The file "tests/test_data/test.csv" should be exist
     The file "tests/test_data/train.csv" should be exist
   End
+
+  It "evaluates a submission"
+    When call geci-ctf evaluate tests/test_data/complete_dataset.csv tests/test_data/test_submission.csv
+    The first line of output should equal "Submission: tests/test_data/test_submission.csv"
+    The second line of output should include "Mean absolute error: 0.4246"
+  End
 End
