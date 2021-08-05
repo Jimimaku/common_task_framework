0ee39eb42e39ee17dce887b7a3edff438d51171d
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-04 18:37:16 -0700

    🧪 Add second data set for testing

--- tests/cli_spec.sh
+++ tests/cli_spec.sh
@@ -4,6 +4,9 @@ Describe "geci-ctf"
             rm --force tests/test_dataset1/example_submission.csv; \
             rm --force tests/test_dataset1/test.csv; \
             rm --force tests/test_dataset1/train.csv; \
+            rm --force tests/test_dataset2/example_submission.csv; \
+            rm --force tests/test_dataset2/test.csv; \
+            rm --force tests/test_dataset2/train.csv; \
             }
   Before "clean"
   After "clean"
@@ -25,5 +28,12 @@ Describe "geci-ctf"
     When call geci-ctf evaluate tests/test_dataset1/complete_dataset.csv tests/test_dataset1 --directory
     The first line of output should equal "| submission                               |   mean_absolute_error |"
     The fourth line of output should include "| tests/test_dataset1/test_submission.csv  |                0.4246 |"
+  End
+
+    It "partitions the complete dataset"
+    geci-ctf init tests/test_dataset2/complete_dataset.csv
+    When call cat init tests/test_dataset2/test.csv
+    The second line of output should equal "B6-2012-09-02,28.1,NA,NA,NA,NA,NA"
+    The third line of output should equal "B6-2012-09-03,32.0,16.2,16.0,10.3,NA,NA"
   End
 End
