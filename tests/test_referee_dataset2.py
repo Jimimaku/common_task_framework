e9d6ce7ee2289f17ab11a67dfc8db64da94318bd
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-10-07 15:24:10 -0700

    ✅ Update tests

--- tests/test_referee_dataset2.py
+++ tests/test_referee_dataset2.py
@@ -2,38 +2,29 @@ from ..common_task_framework import get_submission_list, load_submission, Refere
 import os
 import pandas as pd
 
-path_to_complete_dataset = "tests/test_dataset1/complete_dataset.csv"
+path_to_complete_dataset = "tests/test_dataset2/complete_dataset.csv"
 ctf = Referee(path_to_complete_dataset)
-path_to_submission = "tests/test_dataset1/test_submission.csv"
-path_to_submission_directory = "tests/test_dataset1"
+path_to_submission = "tests/test_dataset2/test_submission.csv"
+path_to_submission_directory = "tests/test_dataset2"
 
 
 def test_load_complete_dataset():
     data = ctf.load_complete_dataset()
     obtained_length = len(data)
-    expected_length = 27
+    expected_length = 10
     assert expected_length == obtained_length
 
 
 def test_get_training_length():
     obtained_length = ctf.get_training_length()
-    expected_length = round(27 * 0.8)
+    expected_length = round(10 * 0.8)
     assert expected_length == obtained_length
 
 
 def test_get_testing_length():
     obtained_length = ctf.get_testing_length()
-    expected_length = round(27 * 0.2)
+    expected_length = round(10 * 0.2)
     assert expected_length == obtained_length
-
-
-def test_get_training_dataset():
-    train = ctf.get_training_dataset()
-    obtained_rows, obtained_cols = train.shape
-    expected_rows = ctf.get_training_length()
-    assert expected_rows == obtained_rows
-    expected_cols = ctf.data.shape[1]
-    assert expected_cols == obtained_cols
 
 
 def test_get_testing_dataset():
@@ -42,100 +33,26 @@ def test_get_testing_dataset():
     expected_rows = ctf.get_testing_length()
     assert expected_rows == obtained_rows
     obtained_column_names = list(test.columns)
-    expected_column_names = ["x"]
+    expected_column_names = ["Peso","Longitud_tarso","Longitud_ala","Longitud_pico","Longitud_pluma_interior_de_la_cola","Longitud_pluma_exterior_de_la_cola"]
     assert obtained_column_names == expected_column_names
-
-
-def test_get_example_submission():
-    example_submission = ctf.get_example_submission()
-    obtained_rows = len(example_submission)
-    expected_rows = ctf.get_testing_length()
-    assert expected_rows == obtained_rows
-    obtained_column_names = list(example_submission.columns)
-    expected_column_names = ["target"]
-    assert expected_column_names == obtained_column_names
-    obtained_example_target = example_submission["target"].iloc[0]
-    assert obtained_example_target >= 0
-
-
-def test_get_behind_the_wall_solution():
-    solution = ctf.get_behind_the_wall_solution()
-    obtained_rows = len(solution)
-    expected_rows = ctf.get_testing_length()
-    assert expected_rows == obtained_rows
-    obtained_column_names = list(solution.columns)
-    expected_column_names = ["target"]
-    assert expected_column_names == obtained_column_names
-    obtained_solution_target = solution["target"].iloc[0]
-    expected_solution_target = ctf.data["target"].iloc[ctf.get_training_length()]
-    assert expected_solution_target == obtained_solution_target
 
 
 def test_get_training_path():
     obtained_path = ctf.get_training_path()
-    expected_path = "tests/test_dataset1/train.csv"
+    expected_path = "tests/test_dataset2/train.csv"
     assert expected_path == obtained_path
 
 
 def test_get_testing_path():
     obtained_path = ctf.get_testing_path()
-    expected_path = "tests/test_dataset1/test.csv"
+    expected_path = "tests/test_dataset2/test.csv"
     assert expected_path == obtained_path
 
 
 def test_get_example_submission_path():
     obtained_path = ctf.get_example_submission_path()
-    expected_path = "tests/test_dataset1/example_submission.csv"
+    expected_path = "tests/test_dataset2/example_submission.csv"
     assert expected_path == obtained_path
-
-
-def test_save_training_dataset():
-    path_to_training = ctf.get_training_path()
-    if os.path.exists(path_to_training):
-        os.remove(path_to_training)
-    ctf.save_training_dataset()
-    assert os.path.exists(path_to_training)
-    obtained_first_column = pd.read_csv(path_to_training).columns[0]
-    expected_first_column = "id"
-    assert expected_first_column == obtained_first_column
-    os.remove(path_to_training)
-
-
-def test_save_testing_dataset():
-    path_to_testing = ctf.get_testing_path()
-    if os.path.exists(path_to_testing):
-        os.remove(path_to_testing)
-    ctf.save_testing_dataset()
-    assert os.path.exists(path_to_testing)
-    obtained_first_column = pd.read_csv(path_to_testing).columns[0]
-    expected_first_column = "id"
-    assert expected_first_column == obtained_first_column
-    os.remove(path_to_testing)
-
-
-def test_save_example_submission():
-    path_to_example_submission = ctf.get_example_submission_path()
-    if os.path.exists(path_to_example_submission):
-        os.remove(path_to_example_submission)
-    ctf.save_example_submission()
-    assert os.path.exists(path_to_example_submission)
-    obtained_first_column = pd.read_csv(path_to_example_submission).columns[0]
-    expected_first_column = "id"
-    assert expected_first_column == obtained_first_column
-    os.remove(path_to_example_submission)
-
-
-def test_init():
-    ctf.init()
-    path_to_training = ctf.get_training_path()
-    assert os.path.exists(path_to_training)
-    os.remove(path_to_training)
-    path_to_testing = ctf.get_testing_path()
-    assert os.path.exists(path_to_testing)
-    os.remove(path_to_testing)
-    path_to_example_submission = ctf.get_example_submission_path()
-    assert os.path.exists(path_to_example_submission)
-    os.remove(path_to_example_submission)
 
 
 def test_load_submission():
@@ -154,8 +71,8 @@ def test_get_mean_absolute_error():
 def test_get_submission_list():
     obtained_submission_list = get_submission_list(path_to_submission_directory)
     expected_submission_list = [
-        "tests/test_dataset1/test_submission.csv",
-        "tests/test_dataset1/test2_submission.csv",
+        "tests/test_dataset2/test_submission.csv",
+        "tests/test_dataset2/test2_submission.csv",
     ]
     assert sorted(expected_submission_list) == sorted(obtained_submission_list)
 
@@ -166,11 +83,11 @@ def test_get_mean_absolute_error_list():
     )
     expected_mean_absolute_error_list = pd.DataFrame(columns=["submission", "mean_absolute_error"])
     expected_mean_absolute_error_list = expected_mean_absolute_error_list.append(
-        {"submission": "tests/test_dataset1/test2_submission.csv", "mean_absolute_error": 0.2446},
+        {"submission": "tests/test_dataset2/test2_submission.csv", "mean_absolute_error": 0.2446},
         ignore_index=True,
     )
     expected_mean_absolute_error_list = expected_mean_absolute_error_list.append(
-        {"submission": "tests/test_dataset1/test_submission.csv", "mean_absolute_error": 0.4246},
+        {"submission": "tests/test_dataset2/test_submission.csv", "mean_absolute_error": 0.4246},
         ignore_index=True,
     )
     pd.testing.assert_frame_equal(
