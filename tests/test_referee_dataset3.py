1be989c42cb834bcf64776c45fb3745502025f5e
Fernando <fernando.alvarez@islas.org.mx>
2021-11-11 10:55:46 -0800

    ✅ Add instance of class inside tests

--- tests/test_referee_dataset3.py
+++ tests/test_referee_dataset3.py
@@ -3,11 +3,10 @@ import pandas as pd
 import pytest
 
 path_to_complete_dataset = "tests/test_dataset3/complete_dataset.csv"
-ctf = Referee(path_to_complete_dataset)
-
 
 def test_exceptions_raised_when_no_values():
     with pytest.raises(
         ValueError, match="^There is a record with no values for any explanatory variable$"
     ):
+        ctf = Referee(path_to_complete_dataset)
         ctf.load_complete_dataset()
