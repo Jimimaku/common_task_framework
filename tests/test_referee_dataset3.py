456e061bba3d8808b16a6ded6deb86cd43e5e8de
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-11-11 12:48:53 -0800

    🚨 Remove unused variable

--- tests/test_referee_dataset3.py
+++ tests/test_referee_dataset3.py
@@ -8,4 +8,4 @@ def test_exceptions_raised_when_no_values():
     with pytest.raises(
         ValueError, match="^There is a record with no values for any explanatory variable$"
     ):
-        ctf = Referee(path_to_complete_dataset)
+        Referee(path_to_complete_dataset)
